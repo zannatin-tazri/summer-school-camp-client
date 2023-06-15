fcc76d6bdcce7ab3651b38c90178f69b14e8e700
zannatin-tazri <zannatintazrizan@gmail.com>
2023-06-16 03:14:42 +0600

    data loaded from database

--- src/pages/Home/Instructor/Instructors.jsx
+++ src/pages/Home/Instructor/Instructors.jsx
@@ -1,11 +1,10 @@
 import { useEffect, useState } from "react";
 
-
 const Instructors = () => {
     const [instructors, setInstructors] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:5000/instructors')
+        fetch('http://localhost:5000/instructor')
             .then(res => res.json())
             .then(data => setInstructors(data))
             .catch(error => console.log(error));
