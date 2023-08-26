c6e474f5c60f1c2b38e0f2c260d1cdcf56f21ff9
zannatin-tazri <zannatintazrizan@gmail.com>
2023-08-26 21:07:35 +0600

    slider spacing margin

--- src/pages/Home/Home/Slider/Slider.jsx
+++ src/pages/Home/Home/Slider/Slider.jsx
@@ -6,27 +6,35 @@ const Slider = () => {
     return (
         <div>
             <div className="carousel w-full h-96">
-  <div id="item1" className="carousel-item w-full p-16">
+
+
+  <div id="item1" className="carousel-item w-full p-12">
     <div className='flex items-center '>
-        <p className='text-2xl text-indigo-950 font-semibold'>Art is the lie that enables us to realize the truth. <br /> - Pablo Picasso</p>
+        <p className='mx-4 text-2xl text-indigo-950 font-semibold '>Art is the lie that enables us to realize the truth. <br /> - Pablo Picasso</p>
     </div>
-    <img  src={slider01} className="w-96 h-full rounded-lg ms-6" />
+    <img  src={slider01} className="w-96 h-full rounded-lg ms-4" />
   </div> 
-  <div id="item1" className="carousel-item w-full p-16">
-    <div className='flex items-center '>
-        <p className='text-2xl text-indigo-950 font-semibold'>Art is the lie that enables us to realize the truth. <br /> - Pablo Picasso</p>
+
+
+  <div id="item2" className="carousel-item w-full p-12">
+    <div className='flex items-center'>
+        <p className=' text-2xl text-indigo-950 font-semibold'>Every artist dips his brush in his own soul, and paints his <br /> own nature into his pictures. <br /> - Henry Ward Beecher</p>
     </div>
-    <img  src={slider02} className="w-96 h-full rounded-lg ms-6" />
+    <img  src={slider02} className="w-96 h-full rounded-lg ms-4" />
   </div> 
-  <div id="item1" className="carousel-item w-full p-16">
-    <div className='flex items-center '>
-        <p className='text-2xl text-indigo-950 font-semibold'>Art is the lie that enables us to realize the truth. <br /> - Pablo Picasso</p>
+
+
+  <div id="item3" className="carousel-item w-full p-16">
+    <div className='mx-4 flex items-center '>
+        <p className='text-2xl text-indigo-950 font-semibold'>Art enables us to find ourselves and lose ourselves <br /> at the same time. <br /> - Thomas Merton</p>
     </div>
-    <img  src={slider03} className="w-96 h-full rounded-lg ms-6" />
+    <img  src={slider03} className="w-80 h-full rounded-lg ms-4" />
   </div> 
-  <div id="item1" className="carousel-item w-full p-16">
+
+  
+  <div id="item4" className="mx-4 carousel-item w-full p-16">
     <div className='flex items-center '>
-        <p className='text-2xl text-indigo-950 font-semibold'>Art is the lie that enables us to realize the truth. <br /> - Pablo Picasso</p>
+        <p className='text-2xl text-indigo-950 font-semibold'>The purpose of art is washing the dust of daily <br /> life off our souls. <br /> - Pablo Picasso</p>
     </div>
     <img  src={slider04} className="w-96 h-full rounded-lg ms-6" />
   </div> 
